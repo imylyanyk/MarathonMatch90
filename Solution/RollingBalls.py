f5eeef617a069a362944d61f3d297712586cadcd
cupidon4uk <mylyanyk.ivan@gmail.com>
2015-12-22 01:28:59 +0200

    modified algorithm, real submission #2
    implemented the following behavior: if we can pull the ball to the right
    place - do it, otherwise try to throw it in a random direction. in
    multiple iterations.

--- Solution/RollingBalls.py
+++ Solution/RollingBalls.py
@@ -1,8 +1,8 @@
-
 # 0 y-
 # 1 x+
 # 2 y+
 # 3 x-
+import random
 
 
 class RollingBalls:
@@ -20,7 +20,28 @@ class RollingBalls:
     def build(self, start):
         n = len(start)
         m = len(start[0])
-        return [[self.calc(i, j, start) for j in range(m)] for i in range(n)]
+        res = [[[j, i, j, i] for j in range(m)] for i in range(n)]
+
+        for i in range(n):
+            for j in range(m):
+                if start[i][j] != '#':
+                    res[i][j][0] = -1 if j == 0 else (res[i][j - 1][0] if start[i][j-1] == '.' else j-1)
+                    res[i][j][3] = -1 if i == 0 else (res[i - 1][j][3] if start[i-1][j] == '.' else i-1)
+
+        for i in range(n - 1, -1, -1):
+            for j in range(m - 1, -1, -1):
+                if start[i][j] != '#':
+                    res[i][j][1] = n if i == n - 1 else (res[i + 1][j][1] if start[i+1][j] == '.' else i+1)
+                    res[i][j][2] = m if j == m - 1 else (res[i][j + 1][2] if start[i][j+1] == '.' else j+1)
+        #
+        # res2 = [[self.calc(i, j, start) for j in range(m)] for i in range(n)]
+        #
+        # for i in range(n):
+        #     for j in range(n):
+        #         if start[i][j] != '#':
+        #             if res2[i][j] != res[i][j]:
+        #                 print('not equal!')
+        return res
 
     def calc(self, x, y, start):
         n = len(start)
@@ -36,33 +57,125 @@ class RollingBalls:
             res[3] -= 1
         return res
 
+    def calcAllowed(self, board):
+        res = 0
+        for s in board:
+            for c in s:
+                if c.isdigit():
+                    res += 1
+        return res
+
     def algo1(self, s, t):
-        walls = self.build(s)
+        #walls = self.build(s)
         # trying to reach within one move
         n = len(s)
         m = len(s[0])
         res = []
-        for i in range(n):
-            for j in range(m):
-                if s[i][j].isdigit() and t[i][j].isdigit() == False:
-                    tmp = walls[i][j]
-                    step = [i, j, 4]
-                    if t[i][tmp[0] + 1].isdigit():
-                        step = [i, tmp[0] + 1, 0]
 
-                    if t[tmp[1] - 1][j].isdigit():
-                        step = [tmp[1] - 1, j, 1]
+        balls = self.calcAllowed(s)
+        allowed = balls * 20
 
-                    if t[i][tmp[2] - 1].isdigit():
-                        step = [i, tmp[2] - 1, 2]
+        prevSize = 0
+        while True:
 
-                    if t[tmp[3] + 1][j].isdigit():
-                        step = [tmp[3] + 1, j, 3]
+            # try to move to 'main' target (same color)
+            for i in range(n):
+                for j in range(m):
+                    if s[i][j].isdigit() and t[i][j] != s[i][j]:  # the ball isn't yet in target
+                        #tmp = walls[i][j]
+                        tmp = self.calc(i, j, s)
+                        step = [i, j, 4]
+                        if t[i][tmp[0] + 1] == s[i][j]:
+                            step = [i, tmp[0] + 1, 0]
 
-                    if step[2] != 4:
-                        res += [str(i) + ' ' + str(j) + ' ' + str(step[2])]
-                        s[step[0]][step[1]] = s[i][j]
-                        s[i][j] = '.'
-                        walls = self.build(s)
-                        continue
+                        if t[tmp[1] - 1][j] == s[i][j]:
+                            step = [tmp[1] - 1, j, 1]
+
+                        if t[i][tmp[2] - 1] == s[i][j]:
+                            step = [i, tmp[2] - 1, 2]
+
+                        if t[tmp[3] + 1][j] == s[i][j]:
+                            step = [tmp[3] + 1, j, 3]
+
+                        if step[2] != 4:
+                            res += [str(i) + ' ' + str(j) + ' ' + str(step[2])]
+                            s[step[0]][step[1]] = s[i][j]
+                            s[i][j] = '.'
+                            #walls = self.build(s)
+                            #continue
+
+            if len(res) + 2 * balls > allowed:
+                # try to move anywhere on board, preferably to destination with score 0.5
+                for i in range(n):
+                    for j in range(m):
+                        if s[i][j].isdigit() and t[i][j].isdigit() == False:
+                            #tmp = walls[i][j]
+                            tmp = self.calc(i, j, s)
+                            step = [i, j, 4]
+                            if t[i][tmp[0] + 1].isdigit():
+                                step = [i, tmp[0] + 1, 0]
+
+                            if t[tmp[1] - 1][j].isdigit():
+                                step = [tmp[1] - 1, j, 1]
+
+                            if t[i][tmp[2] - 1].isdigit():
+                                step = [i, tmp[2] - 1, 2]
+
+                            if t[tmp[3] + 1][j].isdigit():
+                                step = [tmp[3] + 1, j, 3]
+
+                            if step[2] != 4:
+                                res += [str(i) + ' ' + str(j) + ' ' + str(step[2])]
+                                s[step[0]][step[1]] = s[i][j]
+                                s[i][j] = '.'
+                                #walls = self.build(s)
+
+            # otherwise, move randomly on board
+            for i in range(n):
+                for j in range(m):
+                    if s[i][j].isdigit() and t[i][j] != s[i][j]:
+                        #tmp = walls[i][j]
+                        tmp = self.calc(i, j, s)
+                        step = [i, j, 4]
+                        if t[i][tmp[0] + 1].isdigit():
+                            step = [i, tmp[0] + 1, 0]
+
+                        if t[tmp[1] - 1][j].isdigit():
+                            step = [tmp[1] - 1, j, 1]
+
+                        if t[i][tmp[2] - 1].isdigit():
+                            step = [i, tmp[2] - 1, 2]
+
+                        if t[tmp[3] + 1][j].isdigit():
+                            step = [tmp[3] + 1, j, 3]
+
+                        if step[2] == 4 or (len(res) + 2 * balls < allowed):
+                            # can roll it randomly
+                            move = random.randint(0, 3)
+                            if move == 0:
+                                step = [i, tmp[0] + 1, 0]
+                            if move == 1:
+                                step = [tmp[1] - 1, j, 1]
+                            if move == 2:
+                                step = [i, tmp[2] - 1, 2]
+                            if move == 3:
+                                step = [tmp[3] + 1, j, 3]
+
+                            if step[0] == i and step[1] == j:
+                                continue
+
+                            res += [str(i) + ' ' + str(j) + ' ' + str(step[2])]
+                            s[step[0]][step[1]] = s[i][j]
+                            s[i][j] = '.'
+                            #walls = self.build(s)
+
+            if len(res) == prevSize:
+                break
+            if len(res) > allowed:
+                break
+            prevSize = len(res)
+
+        if len(res) > allowed:
+            res = res[:allowed]
+
         return res
